febc716506886a02b2373c06965bbe73dddecdb7
Syed Mahib <syedmahib8@gmail.com>
2025-06-13 23:26:57 +0600

    created the login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import Lottie from "lottie-react";
+import React from "react";
+import LoginLottie from "../assets/Lotties/Login.json"
 
 const Login = () => {
-    return (
-        <div>
-            I am login
+  return (
+    <div className="hero bg-[#E6D3B3] min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <Lottie animationData={LoginLottie} loop={true}></Lottie>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h1 className="text-4xl font-bold text-center">Login now!</h1>
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
